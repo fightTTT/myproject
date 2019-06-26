10dbd707613772c22b29a8826f3439c754b942de
sakumeikou <mail@mail.com>
2019-06-26 17:01:40 +0900

    自己制作

---
+++ 自己制作2019/Project3/common/VECTOR2.h
@@ -0,0 +1,73 @@
+#pragma once
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(const int x,const int y);
+	~Vector2();
+
+	int x;		// Xç¿ïW
+	int y;		// Yç¿ïW
+
+	// ë„ì¸ââéZéq
+	Vector2& operator = (const Vector2& vec);
+
+	// ìYÇ¶éöââéZéq
+	int& operator[](const int i);
+
+	// î‰ärââéZéq
+	bool operator==(const Vector2& vec) const;
+	bool operator!=(const Vector2& vec) const;
+	bool operator<=(const Vector2& vec) const;
+	bool operator< (const Vector2& vec) const;
+	bool operator>=(const Vector2& vec) const;
+	bool operator> (const Vector2& vec) const;
+	bool operator< (const int k)const;
+
+	// íPçÄââéZéq
+	Vector2& operator += (const Vector2& vec);
+	Vector2& operator -= (const Vector2& vec);
+	Vector2& operator *= (const int k);
+	Vector2& operator /= (const int k);
+	Vector2  operator+() const { return (*this); };
+	Vector2  operator-() const { return Vector2(-x,-y); };
+};
+
+// ÉxÉNÉgÉãÇÃââéZ
+// VECTOR2 + int
+template<typename T>
+Vector2 operator+(const Vector2 &u,const T v);
+
+// VECTOR2 - int
+template<typename T>
+Vector2 operator-(const Vector2 &u,const T v);
+
+// VECTOR2 + VECTOR2
+Vector2 operator+(const Vector2 &u, const Vector2 &v);
+
+// VECTOR2 - VECTOR2
+Vector2 operator-(const Vector2 &u, const Vector2 &v);
+
+// VECTOR2 * int
+template<typename T>
+Vector2 operator*(const Vector2 &u,const T v);
+
+// int * VECTOR2
+template<typename T>
+Vector2 operator*(const T u, const Vector2 &v);
+
+Vector2 operator*(const Vector2 u, const Vector2 &v);
+
+// VECTOR2 / int
+template<typename T>
+Vector2 operator/(const Vector2 &u,const T v);
+
+// VECTOR2 / VECTOR2
+Vector2 operator/(const Vector2 &u, const Vector2 &v);
+
+// VECTOR2 % int
+template<typename T>
+Vector2 operator%(const Vector2 &u,const T v);
+
+// VECTOR2 % VECTOR2
+Vector2 operator%(const Vector2 &u, const Vector2 &v);
---
+++ 自己制作2019/Project3/main.cpp
@@ -0,0 +1,11 @@
+#include "DxLib.h"
+#include "SceneMng.h"
+
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+
+	lpSceneMng.Run();
+
+	DxLib_End();	// DX×²ÌŞ×Ø‚ÌI—¹ˆ—
+	return 0;
+}
