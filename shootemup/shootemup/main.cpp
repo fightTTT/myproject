2bd77984340abdbe2c166fe541cddf37373bebc1
sakumeikou <mail@mail.com>
2019-06-28 21:27:50 +0900

    a

--- shootemup/shootemup/main.cpp
+++ shootemup/shootemup/main.cpp
@@ -9,6 +9,8 @@ enum SHOTPT
 	SHOTPT_3WAY,
 	SHOTPT_BARAM,
 	SHOTPT_HOUSYA,
+	SHOTPT_JUZI,
+	SHOTPT_TUIJU,
 	SHOTPT_MAX
 };
 ///当たり判定関数
@@ -36,7 +38,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int skyH = LoadGraph("img/sky.png");
 	int sky2H = LoadGraph("img/sky2.png");
 
-	auto bulletH=LoadGraph("img/bullet.png");
+	auto bulletH=LoadGraph("img/bullet1.png");
 	int playerH[10];
 	LoadDivGraph("img/player.png", 10, 5, 2, 16, 24,playerH );
 
@@ -46,7 +48,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	struct Bullet {
 		Position2 pos;//座標
 		Vector2 vel;//速度
+		int speed;
+		int tuijuCnt;
+		float angle;
 		bool isActive = false;//生きてるか～？
+		bool flag = false;
 	};
 
 	
@@ -62,7 +68,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	SHOTPT shotPT;
 
-	Position2 enemypos(320,25);//敵座標
+	Position2 enemypos(320,50);//敵座標
 	Position2 playerpos(320, 400);//自機座標
 
 	unsigned int frame = 0;//フレーム管理用
@@ -73,6 +79,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int skyy = 0;
 	int skyy2 = 0;
 	int bgidx = 0;
+	int juzicnt = 0;
+	int tuijuCnt = 0;
+	bool stopFlag = false;
+
+	for (auto& b : bullets)
+	{
+		b.speed = 2;
+		b.tuijuCnt = 0;
+	}
+
 	while (ProcessMessage() == 0&&CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
 		ClearDrawScreen();
 
@@ -80,21 +96,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		frameCnt = frame / 60;
 		frameCnt %= 60;
-		if (frameCnt < 20)
+		if (frameCnt < 10)
+		{
+			shotPT = SHOTPT_JUZI;
+		}
+		else if (frameCnt < 13)
+		{
+			shotPT = SHOTPT_MAX;
+		}
+		else if(frameCnt < 20)
+		{
+			shotPT = SHOTPT_TUIJU;
+		}
+		else if (frameCnt < 40)
 		{
 			shotPT = SHOTPT_3WAY;
 		}
-		else if(frameCnt < 30)
-		{
-			shotPT = SHOTPT_HOUSYA;
-		}
-		else if (frameCnt < 40)
+		else if(frameCnt < 50)
 		{
 			shotPT = SHOTPT_BARAM;
 		}
 		else
 		{
-			shotPT = SHOTPT_BARAM;
+			shotPT = SHOTPT_HOUSYA;
+			juzicnt = 0;
+			stopFlag = false;
+
+			for (auto& b : bullets)
+			{
+				b.flag = false;
+			}
 		}
 
 		isDebugMode = keystate[KEY_INPUT_P];
@@ -144,10 +175,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 					b.vel.x = cos(angle) * 1;
 					b.vel.y = sin(angle) * 1;
-
 				}
 				cnt++;
 			}
+			//stopFlag = true;
 		}
 
 		int pidx = (frame/4 % 2)*5+3;
@@ -159,6 +190,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		if (shotPT == SHOTPT_3WAY && !stop)
 		{
+			for (auto& b : bullets)
+			{
+				if (b.isActive)
+				{
+					if (!b.flag)
+					{
+						if (b.tuijuCnt >= 20)
+						{
+							float angle = atan2f(playerpos.y - b.pos.y, playerpos.x - b.pos.x);
+							b.vel.x = cos(angle) * 4;
+							b.vel.y = sin(angle) * 4;
+							b.flag = true;
+						}
+					}
+						
+				}
+			}
+
 			//弾発射
 			if (frame % 15 == 0)
 			{
@@ -180,6 +229,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 					}
 				}
 			}
+
+			
 		}
 		else if (shotPT == SHOTPT_HOUSYA && !stop)
 		{
@@ -232,19 +283,117 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 							b.isActive = true;
 							break;
 
-							/*b.pos = enemypos;
-							float angle = atan2f(playerpos.y - b.pos.y, playerpos.x - b.pos.x);
-							b.vel = { 0,0 };
-
-							b.vel.x = cos(angle + (15 * i * DX_PI / 180)) * 2;
-							b.vel.y = sin(angle + (15 * i * DX_PI / 180)) * 2;
-							b.isActive = true;
-							break;*/
 						}
 					}
 				}
 			}
 		}
+		else if (shotPT == SHOTPT_JUZI && !stop)
+		{
+			for (int i = 0; i < 360 / 90; i++)
+			{
+				if (frame % 7 == 0)
+				{
+					for (auto& b : bullets)
+					{
+
+						if (!b.isActive)
+						{
+
+							b.pos = enemypos;
+
+							b.vel = { 0,0 };
+							b.vel.x = cos(((90 * i * DX_PI + (5 * juzicnt)) / 180)) * 2;
+							b.vel.y = sin(((90 * i * DX_PI + (5 * juzicnt)) / 180)) * 2;
+							/*if (frameCnt >= 5)
+							{
+								juzicnt++;
+								b.vel.x = cos((90 * i * DX_PI + (5*juzicnt) / 180)) * 5;
+								b.vel.y = sin((90 * i * DX_PI + (5 * juzicnt) / 180)) * 5;
+
+							}*/
+							juzicnt++;
+							b.isActive = true;
+							b.angle = ((90 * i * DX_PI) / 180);
+							break;
+						}
+					}
+					for (auto& b : bullets)
+					{
+
+						if (!b.isActive)
+						{
+
+							b.pos = enemypos;
+
+							b.vel = { 0,0 };
+							b.vel.x = cos(((90 * i * DX_PI + (-5 * juzicnt)) / 180)) * 2;
+							b.vel.y = sin(((90 * i * DX_PI + (-5 * juzicnt)) / 180)) * 2;
+							/*if (frameCnt >= 5)
+							{
+								juzicnt++;
+								b.vel.x = cos((90 * i * DX_PI + (5*juzicnt) / 180)) * 5;
+								b.vel.y = sin((90 * i * DX_PI + (5 * juzicnt) / 180)) * 5;
+
+							}*/
+							juzicnt++;
+							b.isActive = true;
+							b.angle = ((90 * i * DX_PI) / 180);
+							break;
+						}
+					}
+				}
+			}
+		}
+		else if(shotPT == SHOTPT_TUIJU && !stop)
+		{ 
+			if (frame % 90 == 0)
+			{
+				for (int i = 0; i < 360 / 15; i++)
+				{
+					for (auto& b : bullets)
+					{
+						if (!b.isActive)
+						{
+							b.pos = enemypos;
+							float angle = atan2f(playerpos.y - b.pos.y, playerpos.x - b.pos.x);
+							b.vel = { 0,0 };
+							b.tuijuCnt = 0;
+							b.vel.x = cos(angle + (15 * i * DX_PI / 180)) * 1.5f;
+							b.vel.y = sin(angle + (15 * i * DX_PI / 180)) * 1.5f;
+							b.isActive = true;
+							break;
+						}
+
+					}
+				}
+			}
+
+			for (auto& b : bullets)
+			{
+				if (b.isActive)
+				{
+					if (b.tuijuCnt < 20)
+					{
+					}
+					else if (b.tuijuCnt < 90)
+					{
+						float angle = atan2f(playerpos.y - b.pos.y, playerpos.x - b.pos.x);
+						b.vel.x = cos(angle) * 1;
+						b.vel.y = sin(angle) * 1;
+					}
+					else if (b.tuijuCnt < 100)
+					{
+						float angle = atan2f(playerpos.y - b.pos.y, playerpos.x - b.pos.x);
+						b.vel.x = cos(angle) * 3;
+						b.vel.y = sin(angle) * 3;
+					}
+					b.tuijuCnt++;
+				}
+				
+			}
+		}
+
 		
 
 		//弾の更新および表示
@@ -259,10 +408,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			{
 				b.pos += b.vel;
 			}
-			//float angle = atan2f(playerpos.y - b.pos.y,playerpos.x - b.pos.x);
+			float angle = atan2f(playerpos.y - b.pos.y,playerpos.x - b.pos.x);
 			//弾の角度をatan2で計算してください。angleに値を入れるんだよオゥ
 			
-			DrawRotaGraph(b.pos.x, b.pos.y,1.0f,0, bulletH, true);
+			DrawRotaGraph(b.pos.x, b.pos.y,0.2f, angle, bulletH, true);
 			
 			if (isDebugMode) {
 				//弾の本体(当たり判定)
@@ -288,7 +437,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		//敵の表示
-		enemypos.x = abs((int)((frame+320) % 1280) - 640);
+		enemypos.x = abs((int)((frame + 320) % 1280) - 640);
 		int eidx = (frame / 4 % 2);
 		DrawRotaGraph(enemypos.x, enemypos.y, 2.0f, 0.0f, enemyH[eidx],true);
 
